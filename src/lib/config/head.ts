eef305f10f91993595b412c14a913dedbe0891b9
藍 <50108258+kwaa@users.noreply.github.com>
2022-01-18 17:08:38 +0800

    disallow baiduspider

--- src/lib/config/head.ts
+++ src/lib/config/head.ts
@@ -3,6 +3,7 @@ export const head = {
     common: [],
     dev: [],
     prod: [
+      '<meta name="baiduspider" content="noindex">',
       '<script async defer data-do-not-track="true" data-website-id="ba1bafad-7768-4723-9395-5cad73a0bf87" src="https://umami.kwaa.dev/umami.js"></script>'
     ]
   },
