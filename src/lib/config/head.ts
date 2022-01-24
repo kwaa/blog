e27f5b7cf004ea45126eb0b4cef00ba9362c26f2
藍 <50108258+kwaa@users.noreply.github.com>
2022-01-24 19:17:49 +0800

    update

--- src/lib/config/head.ts
+++ src/lib/config/head.ts
@@ -3,8 +3,13 @@ export const head = {
     common: [],
     dev: [],
     prod: [
-      '<meta name="baiduspider" content="noindex">',
-      '<script async defer data-do-not-track="true" data-website-id="ba1bafad-7768-4723-9395-5cad73a0bf87" src="https://umami.kwaa.dev/umami.js"></script>'
+      // Webmention.io
+      '<link rel="webmention" href="https://webmention.io/kwaa.dev/webmention" />',
+      '<link rel="pingback" href="https://webmention.io/kwaa.dev/xmlrpc" />',
+      // Umami Analytics
+      '<script async defer data-do-not-track="true" data-website-id="ba1bafad-7768-4723-9395-5cad73a0bf87" src="https://umami.kwaa.dev/umami.js"></script>',
+      // Block Baiduspider
+      '<meta name="baiduspider" content="noindex">'
     ]
   },
   relMe: []
