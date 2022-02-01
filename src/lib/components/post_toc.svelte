37cc42871fab74fc4ac34e9cd85a2b11c19ed511
藍 <50108258+kwaa@users.noreply.github.com>
2022-02-01 22:25:10 +0800

    toc test

--- src/lib/components/post_toc.svelte
+++ src/lib/components/post_toc.svelte
@@ -1,15 +1,50 @@
-<script lang="ts" context="module">
-  export const prerender = true
-</script>
-
 <script lang="ts">
+  import { onMount, onDestroy } from 'svelte'
+  import { browser } from '$app/env'
   import Tree from '$lib/components/post_toc_tree.svelte'
   export let toc: Urara.PostToc[]
+
+  let intersecting: string[] = []
+  let intersectingArticle: boolean = true
+  let bordered: string[] = []
+
+  onMount(() => {
+    if (browser) {
+      if (window.screen.availWidth >= 1280) {
+        const headingsObserver = new IntersectionObserver(
+          headings =>
+            headings.forEach(heading =>
+              heading.isIntersecting
+                ? intersecting.push(heading.target.id)
+                : (intersecting = intersecting.filter(h => h !== heading.target.id))
+            ),
+          { rootMargin: '-64px 0px 0px 0px' }
+        )
+        const articleObserver = new IntersectionObserver(article => (intersectingArticle = article[0].isIntersecting))
+        Array.from(document.querySelectorAll('main h2, main h3, main h4, main h5, main h6')).forEach(element =>
+          headingsObserver.observe(element)
+        )
+        articleObserver.observe(document.getElementsByTagName('main')[0])
+      }
+    }
+  })
+
+  onDestroy(() => {
+    if (browser) {
+      // @ts-ignore: Cannot find name 'headingsObserver'
+      if (typeof headingsObserver !== 'undefined') headingsObserver.disconnect()
+      // @ts-ignore: Cannot find name 'articleObserver'
+      if (typeof headingsObserver !== 'undefined') articleObserver.disconnect()
+    }
+  })
+
+  $: if (intersecting.length > 0) bordered = intersecting
+  $: if (intersectingArticle === false) bordered = []
 </script>
 
-<nav aria-label="TableOfContent" class="sticky top-16 py-8">
+<nav id="post-toc" aria-label="TableOfContent" class="sticky top-16 py-8">
   <Tree
-    count={0}
+    {bordered}
     toc={toc.reduce(
       (acc, heading) => {
         let parent = acc
--- src/lib/components/post_toc_tree.svelte
+++ src/lib/components/post_toc_tree.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
   export let toc: Urara.PostToc
-  export let count: number
+  export let bordered: string[]
   const { title, slug, children } = toc
-  count += 1
 </script>
 
 {#if title}
   <a
     on:click={() => document.getElementById(slug).scrollIntoView({ behavior: 'smooth' })}
-    class="transition-all hover:text-accent"
+    id={`toc-link-${slug}`}
+    class="transition-all hover:!border-primary"
+    class:!border-accent={bordered.includes(slug)}
     href={'javascript:void(0)'}>
     {title}
   </a>
 {/if}
-{#if children}
-  <ul class:pl-4={count > 1}>
+{#if children.length > 0}
+  <ul id={`toc-list-${slug ?? 'root'}`} class="menu">
     {#each children as child}
-      <li>
-        <svelte:self {count} toc={child} />
+      <li
+        id={`toc-item-${child.slug}`}
+        class="hover-bordered"
+        class:bordered={bordered.includes(slug)}>
+        <svelte:self {bordered} toc={child} />
       </li>
     {/each}
   </ul>
