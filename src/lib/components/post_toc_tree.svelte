8051769d36705f2885ace74b81edd070441636bf
藍 <50108258+kwaa@users.noreply.github.com>
2022-02-08 19:41:46 +0800

    update

--- src/lib/components/post_toc_tree.svelte
+++ src/lib/components/post_toc_tree.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
   export let toc: Urara.PostToc
   const { title, slug, children } = toc
-  let hover: boolean = false
 </script>
 
 {#if title}
   <a
     on:click={() => document.getElementById(slug).scrollIntoView({ behavior: 'smooth' })}
-    on:mouseenter={() => hover = true}
-    on:mouseleave={() => hover = false}
     id={`toc-link-${slug}`}
     class="transition-all"
-    class:!border-primary={hover}
     href={'javascript:void(0)'}>
     {title}
   </a>
