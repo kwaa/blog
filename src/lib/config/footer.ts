2d29ca886888a387967a8fab5fce42f87944cbdd
藍 <50108258+kwaa@users.noreply.github.com>
2022-03-19 12:38:03 +0800

    Update footer.ts

--- src/lib/config/footer.ts
+++ src/lib/config/footer.ts
@@ -3,5 +3,5 @@ export const config = {
     '/atom.xml': 'RSS',
     '/sitemap.xml': 'Sitemap'
   },
-  html: '<span>This blog is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0 International License</a>.</span>'
+  html: '<span>This blog is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>.</span>'
 }
