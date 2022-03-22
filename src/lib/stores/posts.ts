de262fb12f346c7e0974350e051c2be8e69cafd6
藍 <50108258+kwaa@users.noreply.github.com>
2022-03-22 17:28:01 +0800

    test

--- src/lib/stores/posts.ts
+++ src/lib/stores/posts.ts
@@ -8,40 +8,40 @@ import { site } from '$lib/config/site'
 import { mode } from '$lib/config/misc'
 import { readable } from 'svelte/store'
 import { genPosts, genTags } from '$lib/utils/posts'
-// export const posts = readable({}, set => {
-//   mode === 'dev'
-//     ? set(genPosts({ postHtml: false }))
-//     : fetch(site.url + 'posts.json')
-//         .then(res => res.json())
-//         .then(data =>
-//           set(
-//             genTags(
-//               Object.entries(data as { [priority: number]: Urara.Post[] }).flatMap(([key, value]) =>
-//                 parseInt(key) > 0 ? value : []
-//               )
-//             )
-//           )
-//         )
-// })
+export const posts = readable({}, set => {
+  mode === 'dev'
+    ? set(genPosts({ postHtml: false }))
+    : fetch(site.url + 'posts.json')
+        .then(res => res.json())
+        .then(data =>
+          set(
+            genTags(
+              Object.entries(data as { [priority: number]: Urara.Post[] }).flatMap(([key, value]) =>
+                parseInt(key) > 0 ? value : []
+              )
+            )
+          )
+        )
+})
 
-export const posts = readable({}, set => set(genPosts()))
+// export const posts = readable({}, set => set(genPosts()))
 
-export const tags = readable({}, set =>
-  set(genTags(Object.entries(genPosts()).flatMap(([key, value]) => (parseInt(key) > 0 ? value : []))))
-)
+// export const tags = readable({}, set =>
+//   set(genTags(Object.entries(genPosts()).flatMap(([key, value]) => (parseInt(key) > 0 ? value : []))))
+// )
 
-// export const tags = readable({}, set => {
-//   mode === 'dev'
-//     ? set(genTags(Object.entries(genPosts({ postHtml: false })).flatMap(([key, value]) => (parseInt(key) > 0 ? value : []))))
-//     : fetch(site.url + 'posts.json')
-//         .then(res => res.json())
-//         .then(data =>
-//           set(
-//             genTags(
-//               Object.entries(data as { [priority: number]: Urara.Post[] }).flatMap(([key, value]) =>
-//                 parseInt(key) > 0 ? value : []
-//               )
-//             )
-//           )
-//         )
-// })
+export const tags = readable({}, set => {
+  mode === 'dev'
+    ? set(genTags(Object.entries(genPosts({ postHtml: false })).flatMap(([key, value]) => (parseInt(key) > 0 ? value : []))))
+    : fetch(site.url + 'posts.json')
+        .then(res => res.json())
+        .then(data =>
+          set(
+            genTags(
+              Object.entries(data as { [priority: number]: Urara.Post[] }).flatMap(([key, value]) =>
+                parseInt(key) > 0 ? value : []
+              )
+            )
+          )
+        )
+})
