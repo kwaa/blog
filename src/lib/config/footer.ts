4cb4f5f212cb61c23e00df74770eb4c35fb7179e
藍 <50108258+kwaa@users.noreply.github.com>
2022-03-29 10:00:57 +0800

    update

--- src/lib/config/footer.ts
+++ src/lib/config/footer.ts
@@ -3,5 +3,5 @@ export const config = {
     '/atom.xml': 'Feed',
     '/sitemap.xml': 'Sitemap'
   },
-  html: '<span>This blog is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a></span>'
+  html: '<span>All original text is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a></span>'
 }
