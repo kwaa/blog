3c70e6cba42e2a30ffeda5d468e2e6e9d292937a
kwaa <50108258+kwaa@users.noreply.github.com>
2022-04-13 21:18:21 +0800

    remote follow update

--- src/lib/components/extra/follow.svelte
+++ src/lib/components/extra/follow.svelte
@@ -1,28 +1,63 @@
 <script lang="ts">
-  const follow = async e => {
-    let account = new FormData(e.target).get('account')
-    if (account.startsWith('@')) account = account.slice(1)
-    const subscribe = await fetch(`https://${account.split('@')[1]}/.well-known/webfinger?resource=acct:${account}`, {
-      headers: { Accept: 'application/jrd+json' }
-    })
-      .then(res => res.json())
-      .then(({ links }) => links.find(link => link.rel === 'http://ostatus.org/schema/1.0/subscribe').template)
-      .then(template => template.replace('{uri}', `dotdev@kwaa.moe`))
-    window.location.href = subscribe
-  }
+  import IconPaperAirplane from '~icons/heroicons-outline/paper-airplane'
+  let warning: boolean = false
+  let error: string = ''
+  let input: string = ''
+  const follow = async (event: Event, account = new FormData(event.target as HTMLFormElement).get('account') as string) =>
+    await fetch(
+      account.startsWith('@')
+        ? `https://${account.split('@')[2]}/.well-known/webfinger?resource=acct:${account.slice(1)}`
+        : `https://${account.split('@')[1]}/.well-known/webfinger?resource=acct:${account}`,
+      {
+        headers: { Accept: 'application/jrd+json' }
+      }
+    )
+      .then(res => {
+        if (res.ok) {
+          error = ''
+          return res.json()
+        } else {
+          error = res.status + res.statusText
+          console.log(error)
+          throw Error(res.status + res.statusText)
+        }
+      })
+      .then(
+        ({ links }) => links.find((link: { rel: string }) => link.rel === 'http://ostatus.org/schema/1.0/subscribe').template
+      )
+      .then(template => (window.location.href = template.replace('{uri}', `dotdev@kwaa.moe`)))
+      .catch(error => console.error(error))
+  $: if (input) warning = input.includes('@') && input.includes('.') ? false : true
 </script>
 
 <input type="checkbox" id="remote-follow" class="modal-toggle" />
 <label for="remote-follow" class="modal modal-bottom sm:modal-middle cursor-pointer">
   <div class="modal-box relative" for="">
     <form on:submit|preventDefault={follow} class="form-control gap-2">
-      <label class="label py-0">
-        <span class="label-text">Your account ID</span>
-      </label>
+      <div class="label py-0">
+        <span class="label-text">Your fediverse account ID:</span>
+      </div>
       <label class="input-group">
-        <input type="text" id="account" name="account" placeholder="dotdev@kwaa.moe" class="input input-bordered flex-1" />
-        <button type="submit" class="btn">Go</button>
+        <input
+          bind:value={input}
+          type="text"
+          id="account"
+          name="account"
+          placeholder="username@instance.tld"
+          class:input-warning={warning}
+          class:input-error={error.length > 0}
+          class="input input-bordered flex-1" />
+        <button type="submit" class="btn btn-square">
+          <IconPaperAirplane class="h-6 w-6 rotate-90" />
+        </button>
       </label>
+      <div class="label py-0">
+        {#if error.length > 0}
+          <span class="label-text-alt text-error">
+            {error}{#if error === '404'}: Couldn't find user{/if}
+          </span>
+        {/if}
+      </div>
     </form>
   </div>
 </label>
