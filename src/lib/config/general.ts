f395ec0adbeaf10382c5e8a5c19cdfd5e6813a27
kwaa <50108258+kwaa@users.noreply.github.com>
2022-04-14 22:45:00 +0800

    microsub

--- src/lib/config/general.ts
+++ src/lib/config/general.ts
@@ -52,6 +52,8 @@ export const head: HeadConfig = {
       // IndieAuth
       '<link rel="authorization_endpoint" href="https://indieauth.com/auth">',
       '<link rel="token_endpoint" href="https://tokens.indieauth.com/token">',
+      // Microsub
+      '<link rel="microsub" href="https://aperture.p3k.io/microsub/741">',
       // Umami Analytics
       '<script async defer data-do-not-track="true" data-website-id="ba1bafad-7768-4723-9395-5cad73a0bf87" src="https://umami.kwaa.dev/umami.js"></script>',
       // Block Baiduspider
