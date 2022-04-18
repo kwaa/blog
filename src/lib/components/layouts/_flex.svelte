844bc2d0e51ca83a2309708956be3e7fe90acbc2
kwaa <50108258+kwaa@users.noreply.github.com>
2022-04-18 23:21:46 +0800

    remove transition

--- src/lib/components/layouts/_flex.svelte
+++ src/lib/components/layouts/_flex.svelte
@@ -1,20 +1,22 @@
-<script lang="ts">
+<!-- <script lang="ts">
   import { onMount } from 'svelte'
   let loaded = false
   onMount(() => (loaded = true))
-</script>
+</script> -->
 
 <div class="flex flex-col flex-nowrap justify-center xl:flex-row xl:flex-wrap">
   <div
-    class="flex-1 w-full max-w-screen-md order-first  transition-all duration-[400ms] xl:delay-[600ms] ease-out transform mx-auto xl:mr-0 {loaded
+    class="flex-1 w-full max-w-screen-md xl:order-last transition-all duration-[400ms] xl:delay-[600ms] ease-out transform mx-auto xl:mr-0">
+    <!-- class="flex-1 w-full max-w-screen-md order-first  transition-all duration-[400ms] xl:delay-[600ms] ease-out transform mx-auto xl:mr-0 {loaded
       ? 'translate-x-0 opacity-100'
-      : 'translate-x-[100vw] xl:translate-x-96 opacity-0'}">
+      : 'translate-x-[100vw] xl:translate-x-96 opacity-0'}" -->
     <slot name="left" />
   </div>
   <div
-    class="flex-1 w-full max-w-screen-md xl:order-last transition-all duration-[400ms] xl:delay-[600ms] ease-out transform mx-auto xl:mr-0 {loaded
+    class="flex-1 w-full max-w-screen-md xl:order-last transition-all duration-[400ms] xl:delay-[600ms] ease-out transform mx-auto xl:mr-0">
+    <!-- class="flex-1 w-full max-w-screen-md xl:order-last transition-all duration-[400ms] xl:delay-[600ms] ease-out transform mx-auto xl:mr-0 {loaded
       ? 'translate-x-0 opacity-100'
-      : '-translate-x-[100vw] xl:-translate-x-96 opacity-0'}">
+      : '-translate-x-[100vw] xl:-translate-x-96 opacity-0'}" -->
     <slot name="right" />
   </div>
   <div class="flex-none w-full max-w-screen-md mx-auto xl:mx-0">
