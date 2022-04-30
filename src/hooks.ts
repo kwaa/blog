0badd3d3f1ea0f59a8e0262fa3cff5c345572e82
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-04-30 22:59:13 +0800

    fix

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,41 +1,14 @@
 import type { Handle } from '@sveltejs/kit'
 import { prerendering } from '$app/env'
-import { minify } from 'html-minifier'
 import { site } from '$lib/config/site'
-
-const minificationOptions = {
-  collapseBooleanAttributes: true,
-  collapseWhitespace: true,
-  conservativeCollapse: true,
-  decodeEntities: true,
-  html5: true,
-  ignoreCustomComments: [/^#/],
-  minifyCSS: true,
-  minifyJS: true,
-  removeAttributeQuotes: true,
-  removeComments: true,
-  removeOptionalTags: true,
-  removeRedundantAttributes: true,
-  removeScriptTypeAttributes: true,
-  removeStyleLinkTypeAttributes: true,
-  sortAttributes: true,
-  sortClassName: true
-}
 
 export const handle: Handle = async ({ event, resolve }) =>
   await resolve(event, {
     transformPage: ({ html }) =>
-      // html
-      //   .replace('<html lang="en">', `<html lang="${site.lang ?? 'en'}">`)
-      //   .replace(
-      //     /<link rel="modulepreload" href="(\/_app\/chunks\/vendor-[^.]+\.js)">/,
-      //     (_, url) => `<script defer async src="${url}">`
-      //   )
-    (prerendering
-      ? minify(html.replace('<html lang="en">', `<html lang="${site.lang ?? 'en'}">`), minificationOptions)
-      : html.replace('<html lang="en">', `<html lang="${site.lang ?? 'en'}">`)
-    ).replace(
-      /<link rel="modulepreload" href="(\/_app\/chunks\/vendor-[^.]+\.js)">/,
-      (_, url) => `<script defer async src="${url}">`
-    )
+      html
+        .replace('<html lang="en">', `<html lang="${site.lang ?? 'en'}">`)
+        .replace(
+          /<link rel="modulepreload" href="(\/_app\/chunks\/vendor-[^.]+\.js)">/,
+          (_, url) => `<script defer async src="${url}">`
+        )
   })
