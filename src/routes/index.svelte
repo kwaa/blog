005100f5129886c7cf4037db4ae4eb6d9dcebadc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-04-30 23:41:05 +0800

    update translate

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -108,7 +108,7 @@
           <div
             in:fly={{ x: index % 2 ? 100 : -100, duration: 300, delay: 500 }}
             out:fly={{ x: index % 2 ? -100 : 100, duration: 300 }}
-            class="rounded-box transition-all duration-500 ease-in-out hover:z-30 hover:shadow-lg md:shadow-xl md:hover:shadow-2xl md:hover:-translate-y-1">
+            class="rounded-box transition-all duration-500 ease-in-out hover:z-30 hover:shadow-lg md:shadow-xl md:hover:shadow-2xl md:hover:-translate-y-0.5">
             <Post {post} loading={index < 5 ? 'eager' : 'lazy'} />
           </div>
         {/each}
