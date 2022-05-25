cd1acbb6df89b4303f6f79af59d720e3ffbca4c1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-05-25 16:24:33 +0800

    update

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,14 +1,7 @@
 import type { Handle } from '@sveltejs/kit'
-import { prerendering } from '$app/env'
 import { site } from '$lib/config/site'
 
 export const handle: Handle = async ({ event, resolve }) =>
   await resolve(event, {
-    transformPage: ({ html }) =>
-      html
-        .replace('<html lang="en">', `<html lang="${site.lang ?? 'en'}">`)
-        .replace(
-          /<link rel="modulepreload" href="(\/_app\/chunks\/vendor-[^.]+\.js)">/,
-          (_, url) => `<script defer async src="${url}">`
-        )
+    transformPage: ({ html }) => html.replace('<html lang="en">', `<html lang="${site.lang ?? 'en'}">`)
   })
