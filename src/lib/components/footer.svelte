16c601e291910a2058220aec0f17b49abc306b9b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-05-25 19:59:52 +0800

    Update footer.svelte

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -33,7 +33,7 @@
         rel="noopener external"
         target="_blank"
         class="tooltip tooltip-secondary hover:text-secondary"
-        data-tip="🌸 [Testing] - Based on MDsveX & SvelteKit 🌸"
+        data-tip="🌸 [α] - Based on MDsveX & SvelteKit 🌸"
         href="https://github.com/importantimport/urara">
         Urara
       </a>
