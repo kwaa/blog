e388d7ebba048a4c2ace642436d14246efacf1cc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-06-03 15:46:34 +0800

    update glob

--- svelte.config.js
+++ svelte.config.js
@@ -61,7 +61,7 @@ export default /** @type {import('@sveltejs/kit').Config} */ {
           scope: '/',
           base: '/',
           workbox: {
-            globPatterns: ['**/*.*', '*.*'],
+            globPatterns: ['**/*.{js,css,html,png,webp,avif,svg,webmanifest}', 'index.html'],
             globIgnores: ['**/sw*', '**/workbox-*']
           }
         })
