d5be89f458e1413032afe0c70042eee30f5ff77e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-07-16 02:31:41 +0800

    update

--- src/lib/components/post_action.svelte
+++ src/lib/components/post_action.svelte
@@ -4,7 +4,9 @@
 </script>
 
 <div class="sticky top-24 hidden xl:flex flex-col gap-4 w-fit h-[calc(100vh-12rem)] ml-auto mr-8 my-8 justify-center">
-  {#each Object.values(actions) as action}
-    <svelte:component this={action.default} {post} />
-  {/each}
+  {#if Object.keys(actions).length}
+    {#each Object.values(actions) as action}
+      <svelte:component this={action.default} {post} />
+    {/each}
+  {/if}
 </div>
