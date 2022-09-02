90dd09f7cf2dc8fdd2d0d02e73c875229c9d865a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-09-02 12:40:43 +0800

    color search prototype

--- src/lib/components/header_search.svelte
+++ src/lib/components/header_search.svelte
@@ -1,6 +1,33 @@
 <script lang="ts">
+  import { dev } from '$app/env'
   import { site } from '$lib/config/site'
   import { header as headerConfig } from '$lib/config/general'
+  import { hslToHex } from '$lib/utils/color'
+
+  const b1 = hslToHex(
+    ...(getComputedStyle(document.documentElement)
+      .getPropertyValue('--b1')
+      .slice(dev ? 1 : 0)
+      .replaceAll('%', '')
+      .split(' ')
+      .map(Number) as [number, number, number])
+  )
+  const bc = hslToHex(
+    ...(getComputedStyle(document.documentElement)
+      .getPropertyValue('--bc')
+      .slice(dev ? 1 : 0)
+      .replaceAll('%', '')
+      .split(' ')
+      .map(Number) as [number, number, number])
+  )
+  const p = hslToHex(
+    ...(getComputedStyle(document.documentElement)
+      .getPropertyValue('--p')
+      .slice(dev ? 1 : 0)
+      .replaceAll('%', '')
+      .split(' ')
+      .map(Number) as [number, number, number])
+  )
 </script>
 
 <form
@@ -16,6 +43,16 @@
     type="hidden"
     name={headerConfig.search.provider === 'duckduckgo' ? 'sites' : 'sitesearch'}
     value={site.protocol + site.domain} />
+  {#if headerConfig.search.provider === 'duckduckgo'}
+    <!-- header -->
+    <input type="hidden" name="kj" value={b1} />
+    <!-- background -->
+    <input type="hidden" name="k7" value={b1} />
+    <!-- text -->
+    <input type="hidden" name="k8" value={bc} />
+    <!-- link -->
+    <input type="hidden" name="k9" value={p} />
+  {/if}
   <button type="submit" class="btn btn-square btn-ghost ml-2">
     <span class="i-heroicons-outline-search" />
   </button>
