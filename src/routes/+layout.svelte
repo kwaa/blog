ceb9491872238e41e729a1e9caaf512b40714e10
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-09-03 12:08:28 +0800

    temporarily disable pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import type { LayoutData } from './$types'
-  import { onMount } from 'svelte'
-  import { browser, dev } from '$app/environment'
+  // import { onMount } from 'svelte'
+  // import { browser, dev } from '$app/environment'
   import { fly } from 'svelte/transition'
   import { genTags } from '$lib/utils/posts'
   import { posts, tags } from '$lib/stores/posts'
-  import { registerSW } from 'virtual:pwa-register'
+  // import { registerSW } from 'virtual:pwa-register'
   import Head from '$lib/components/head_static.svelte'
   import Header from '$lib/components/header.svelte'
   import 'uno.css'
@@ -15,15 +15,15 @@
   
   posts.set(data.res)
   tags.set(genTags(data.res))
-  onMount(
-    () =>
-      !dev &&
-      browser &&
-      registerSW({
-        onRegistered: r => r && setInterval(async () => await r.update(), 198964),
-        onRegisterError: error => console.error(error)
-      })
-  )
+  // onMount(
+  //   () =>
+  //     !dev &&
+  //     browser &&
+  //     registerSW({
+  //       onRegistered: r => r && setInterval(async () => await r.update(), 198964),
+  //       onRegisterError: error => console.error(error)
+  //     })
+  // )
 </script>
 
 <Head />
