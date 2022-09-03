86e7c8607be04128655fb962cc5963c435f07a87
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-09-03 13:55:11 +0800

    update

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,7 +4,7 @@
   import Footer from '$lib/components/footer.svelte'
 </script>
 
-<Head page={{ title: $page.status.toString() ?? '404', path: $page.url.pathname }} />
+<Head page={{ title: $page.status ?? '404', path: $page.url.pathname }} />
 
 <div class="flex flex-col flex-nowrap justify-center xl:flex-row xl:flex-wrap">
   <div class="flex-none w-full max-w-screen-md mx-auto xl:mx-0">
@@ -16,7 +16,7 @@
         <h1 class="opacity-20 text-6xl md:text-[12rem] -mt-2 mb-0">
           {$page.status ?? '404'}
         </h1>
-        <h2 class="-mt-12 md:-mt-24">{$page.error.message ?? 'Not found'}</h2>
+        <h2 class="-mt-12 md:-mt-24">{$page.error?.message ?? 'Not found'}</h2>
         <div class="card-actions">
           <a href="/" class="btn btn-neutral no-underline shadow-xl hover:shadow-2xl mt-8">
             <span class="i-heroicons-outline-home -ml-1 mr-2" />
