3a20062c4bcb3c954eff67fc0d101b5a31a87bb4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-11-10 14:26:44 +0800

    warnMissingId

--- svelte.config.ts
+++ svelte.config.ts
@@ -25,6 +25,9 @@ export default defineConfig({
           assets: 'build',
           fallback: undefined
         }),
+    prerender: {
+      handleMissingId: 'warn'
+    },
     csp: { mode: 'auto' }
   }
 })
